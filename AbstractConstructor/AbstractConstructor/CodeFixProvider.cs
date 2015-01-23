43199e5a2691fad8ca284cb9393c3c9340406e45
Filip W <wojcieszyn@gmail.com>
2015-01-23 21:38:09 +0100

    Update CodeFixProvider.cs

--- AbstractConstructor/AbstractConstructor/CodeFixProvider.cs
+++ AbstractConstructor/AbstractConstructor/CodeFixProvider.cs
@@ -32,7 +32,6 @@ namespace AbstractConstructor
         {
             var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
 
-            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
             var diagnostic = context.Diagnostics.First();
             var diagnosticSpan = diagnostic.Location.SourceSpan;
 
